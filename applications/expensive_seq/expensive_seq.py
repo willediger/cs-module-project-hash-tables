4160d94af656b3061c2944c39dddd27b5a8c4b89
Will Ediger <will.ediger@gmail.com>
2020-07-22 22:51:18 -0500

    expensive sequence implementation

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,8 +1,17 @@
 # Your code here
 
-
+# exps(x, y, z) =
+#      if x <= 0: y + z
+#      if x >  0: exps(x-1,y+1,z) + exps(x-2,y+2,z*2) + exps(x-3,y+3,z*3)
+seq = {}
 def expensive_seq(x, y, z):
-    # Your code here
+    if x <= 0:
+        return y + z
+    else:
+        if (x, y, z) not in seq:
+            seq[(x, y, z)] = expensive_seq(x-1, y+1, z) + expensive_seq(x-2, y+2, z*2) + expensive_seq(x-3, y+3, z*3)
+        return seq[(x, y, z)]
+
 
 
 
