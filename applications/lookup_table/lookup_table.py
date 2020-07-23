3e050db4b6cadda9ae2c2f2449402323cadf6528
Will Ediger <will.ediger@gmail.com>
2020-07-22 22:34:26 -0500

    slow fun memoized implementation

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,5 +1,8 @@
 # Your code here
 import random
+import math
+
+fun = {}
 
 def slowfun_too_slow(x, y):
     v = math.pow(x, y)
@@ -14,8 +17,15 @@ def slowfun(x, y):
     Rewrite slowfun_too_slow() in here so that the program produces the same
     output, but completes quickly instead of taking ages to run.
     """
-    # Your code here
+    if (x, y) not in fun:
+        v = math.pow(x, y)
+        v = math.factorial(v)
+        v //= (x + y)
+        v %= 982451653
 
+        fun[(x, y)] = v
+
+    return fun[(x, y)]
 
 
 # Do not modify below this line!
@@ -23,4 +33,4 @@ def slowfun(x, y):
 for i in range(50000):
     x = random.randrange(2, 14)
     y = random.randrange(3, 6)
-    print(f'{i}: {x},{y}: {slowfun(x, y)}')
+    print(f'{i}: {x},{y}: {slowfun_too_slow(x, y)}')
